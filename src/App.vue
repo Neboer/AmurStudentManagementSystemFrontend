e0362cabcf8288f784f2e3308afc7444c215dc70
Neboer <rubinposter@gmail.com>
2025-01-09 19:37:37 +0800

    首次提交，从https://github.com/element-plus/element-plus-vite-starter开始

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <el-config-provider namespace="ep">
+    <BaseHeader />
+    <BaseMenu />
+    <div class="main-container flex">
+      <BaseSide />
+      <div w="full" py="4">
+        <RouterView />
+      </div>
+    </div>
+  </el-config-provider>
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: var(--ep-text-color-primary);
+}
+
+.main-container {
+  height: calc(100vh - var(--ep-menu-item-height) - 4px);
+}
+</style>
