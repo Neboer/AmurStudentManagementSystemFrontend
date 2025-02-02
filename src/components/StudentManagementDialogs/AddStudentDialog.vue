f88c824a8192ab9c3fe092328277f8763fa35e64
Neboer <rubinposter@gmail.com>
2025-02-02 20:41:25 +0800

    成功实现了AddStudentDialog模块，并和StudentManagement页面整合在一起。

--- src/components/StudentManagementDialogs/AddStudentDialog.vue
+++ src/components/StudentManagementDialogs/AddStudentDialog.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import type { AddStudentDialogForm } from '~/components/StudentManagementDialogs/StudentForm'
 import axios from 'axios'
-import { defineModel, nextTick, ref } from 'vue'
-import { default_add_student_dialog_form } from '~/components/StudentManagementDialogs/StudentForm'
+import { defineExpose, defineModel, ref, reactive, watch } from 'vue'
+// import { default_add_student_dialog_form } from '~/components/StudentManagementDialogs/StudentForm'
 import axios_element_handle_error from '~/snippets/axios_handle_error'
 
 const student_dialog_visible = defineModel<boolean>('student_dialog_visible', {
@@ -10,27 +9,61 @@ const student_dialog_visible = defineModel<boolean>('student_dialog_visible', {
     default: false,
 })
 
-const student_dialog_form = defineModel<AddStudentDialogForm>('student_dialog_form', {
-    type: Object,
-    default: () => default_add_student_dialog_form,
+export interface AddStudentDialogForm {
+    name: string
+    phone_number: string
+    init_password: string
+    set_init_password_same_as_phone_number: boolean
+}
+
+const student_dialog_form = reactive<AddStudentDialogForm>({
+    name: '',
+    phone_number: '',
+    init_password: '',
+    set_init_password_same_as_phone_number: true,
+})
+
+defineExpose({
+    student_dialog_form,
 })
 
 const loading = ref(false)
 
+
 function reset_student_dialog_form() {
-    student_dialog_form.value = default_add_student_dialog_form
+    student_dialog_form.name = ''
+    student_dialog_form.phone_number = ''
+    student_dialog_form.init_password = ''
+    student_dialog_form.set_init_password_same_as_phone_number = true
 }
+
+const emit = defineEmits<{
+  (e: 'onAfterAddStudent'): void
+}>()
 
 async function add_student() {
     await axios_element_handle_error(async () => {
         return await axios.post('/api/student', {
-            name: student_dialog_form.value.name,
-            phone_number: student_dialog_form.value.phone_number,
-            init_password: student_dialog_form.value.init_password,
+            name: student_dialog_form.name,
+            phone_number: student_dialog_form.phone_number,
+            init_password: student_dialog_form.init_password,
         })
     }, '添加成功', loading)
     reset_student_dialog_form()
+    student_dialog_visible.value = false
+    emit('onAfterAddStudent')
 }
+
+function on_exit_dialog() {
+    reset_student_dialog_form()
+    student_dialog_visible.value = false
+}
+
+watch(student_dialog_form, (value) => {
+    if (student_dialog_form.set_init_password_same_as_phone_number) {
+        student_dialog_form.init_password = student_dialog_form.phone_number
+    }
+})
 </script>
 
 <template>
@@ -42,16 +75,19 @@ async function add_student() {
             <el-form-item label="手机号码" label-width="140px" required>
                 <el-input v-model="student_dialog_form.phone_number" />
             </el-form-item>
-            <el-form-item label="初始密码" label-width="140px" required :disabled="student_dialog_form.set_init_password_same_as_phone_number">
-                <el-input v-model="student_dialog_form.init_password" />
+            <el-form-item label="初始密码" label-width="140px" required>
+                <el-input v-model="student_dialog_form.init_password"
+                    :disabled="student_dialog_form.set_init_password_same_as_phone_number" />
             </el-form-item>
-            <el-checkbox v-model="student_dialog_form.set_init_password_same_as_phone_number" name="type">
-                设置初始密码与手机号相同
-            </el-checkbox>
+            <el-form-item>
+                <el-checkbox v-model="student_dialog_form.set_init_password_same_as_phone_number" name="type">
+                    设置初始密码与手机号相同
+                </el-checkbox>
+            </el-form-item>
         </el-form>
         <template #footer>
             <div class="dialog-footer">
-                <el-button @click="(student_dialog_visible = false) && reset_student_dialog_form()">
+                <el-button @click="on_exit_dialog">
                     取消
                 </el-button>
                 <el-button type="primary" :loading="loading" @click="add_student">
@@ -62,6 +98,4 @@ async function add_student() {
     </el-dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/admin/StudentManagement.vue
+++ src/pages/admin/StudentManagement.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
-import type { AddStudentDialogForm } from '~/components/StudentManagementDialogs/StudentForm.ts'
 import axios from 'axios'
-import { onMounted, reactive, ref } from 'vue'
-import { default_add_student_dialog_form } from '~/components/StudentManagementDialogs/StudentForm'
+import { onMounted, ref } from 'vue'
 
 const student_table_data = ref([])
 const table_loading = ref(true)
@@ -19,7 +17,6 @@ onMounted(() => {
 })
 
 const student_dialog_visible = ref<boolean>(false)
-const student_dialog_form = reactive<AddStudentDialogForm>(default_add_student_dialog_form)
 // Student: id?: number     name: string     phone_number: string     password: string
 </script>
 
@@ -27,7 +24,8 @@ const student_dialog_form = reactive<AddStudentDialogForm>(default_add_student_d
     <el-button type="primary" @click="student_dialog_visible = true">
         添加学生
     </el-button>
-    <add-student-dialog v-model:student_dialog_visible="student_dialog_visible" v-model:student_dialog_form="student_dialog_form" />
+    <add-student-dialog v-model:student_dialog_visible="student_dialog_visible"
+        @on-after-add-student="refresh_student_table" />
     <el-table v-loading="table_loading" :data="student_table_data" stripe style="width: 100%" empty-text="没有学生">
         <el-table-column prop="name" label="姓名" width="180" />
         <el-table-column prop="phone_number" label="电话号码" />
